6d7dd05ee5123d30306826dacbba4de955c13cf9
Gamer477 <jomohamed87@gmail.com>
2021-10-04 14:08:33 +0200

    updated one

---
+++ lib/inpursettings.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class InputWidgets extends StatelessWidget {
+
+  InputWidgets({required this.labelText,required this.iconType,
+    required this.regexptype,required this.obsecuringText,required this.keyboadtype});
+  final String labelText;
+  final IconData iconType;
+  final bool obsecuringText;
+  final RegExp regexptype;
+  final TextInputType keyboadtype;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300.0,
+      child: TextFormField(
+        keyboardType: keyboadtype,
+        obscureText: obsecuringText,
+        decoration: InputDecoration(
+          labelText: labelText,
+          icon: Icon(iconType),
+        ),
+        validator: (value) {
+          if (value!.isEmpty ||
+              !regexptype
+                  .hasMatch(value)) {
+            return 'Enter a valid $labelText!';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
